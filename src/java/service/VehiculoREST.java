e6587a4ec5fcfdfcb2a9d80866231812bcf29445
omar <multitartanga@gmail.com>
2025-01-16 09:00:49 +0100

    user rest fixing

--- src/java/service/VehiculoREST.java
+++ src/java/service/VehiculoREST.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package service;
 
 import entities.Vehiculo;
@@ -10,17 +5,12 @@ import exception.CreateException;
 import exception.DeleteException;
 import exception.SelectException;
 import exception.UpdateException;
-import java.time.format.DateTimeFormatter;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.swing.text.DateFormatter;
-import javax.ws.rs.Consumes;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.DELETE;
@@ -32,11 +22,6 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-
-/**
- *
- * @author 2dam
- */
 
 @Stateless
 @Path("vehiculo")
@@ -114,38 +99,6 @@ public class VehiculoREST extends AbstractFacade<Vehiculo> {
                 .setParameter("matricula", "%" + matricula + "%")
                 .getResultList();
     }
-
-    @GET
-    @Path("dates")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Vehiculo> findByDateRange(@QueryParam("startDate") String startDate,
-            @QueryParam("endDate") String endDate) throws SelectException {
-        if (startDate == null && endDate == null) {
-            return findAll();
-        }
-
-        String queryStr = "SELECT v FROM Vehiculo v WHERE 1=1";
-        if (startDate != null) {
-            queryStr += " AND v.registrationDate >= :startDate";
-        }
-        if (endDate != null) {
-            queryStr += " AND v.registrationDate <= :endDate";
-        }
-
-        // Declaración explícita del tipo de variable
-        javax.persistence.TypedQuery<Vehiculo> query = em.createQuery(queryStr, Vehiculo.class);
-/*
-        DateFormatter formatter = DateFormatter.ofPattern("dd-MM-yyyy");
-        if (startDate != null) {
-            Date start = Date.parse(startDate, formatter);
-            query.setParameter("startDate", java.sql.Date.valueOf(start));
-        }
-        if (endDate != null) {
-            Date end = Date.parse(endDate, formatter);
-            query.setParameter("endDate", java.sql.Date.valueOf(end));
-        }
-*/
-        return query.getResultList();
 
     // Method to filter vehicles by ITVDATE
     @GET
